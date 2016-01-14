af72ead10e06a59fbdc964f0fb0777435076cb4d
Gerardo Martinez <gmart_nez@yahoo.com.mx>
2016-01-14 16:56:58 -0600

    Add Solution BinaryTree

---
+++ BinaryTree/BinaryTree/Tree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class Tree<TItem> where TItem : IComparable<TItem>
+    {
+        public TItem NodeData { get; set; }
+        public Tree<TItem> LeftTree { get; set; }
+        public Tree<TItem> RightTree { get; set; }
+
+        public Tree(TItem nodeValue)
+        {
+            this.NodeData = nodeValue;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+
+        public void Insert(TItem newItem)
+        {
+            TItem currentNodeValue = this.NodeData;
+            if (currentNodeValue.CompareTo(newItem) > 0)
+            {
+                // Insert the new item into the left subtree
+                if (LeftTree == null)
+                {
+                    this.LeftTree = new Tree<TItem>(newItem);
+                } 
+                else
+                {
+                    this.LeftTree.Insert(newItem);
+                }
+            }
+            else
+            {
+                // Insert the new item into the right subtree
+                if (RightTree == null)
+                {
+                    this.RightTree = new Tree<TItem>(newItem);
+                }
+                else
+                {
+                    this.RightTree.Insert(newItem);
+                }
+            }
+        }
+    }
+}
