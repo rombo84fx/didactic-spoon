c0f686504fc28b5042daa658be85fba0d1bc87c7
rombo84fx <gmart_nez@yahoo.com.mx>
2016-01-24 22:25:23 -0600

    Create the TreeEnumerator Class

---
+++ BinaryTree/BinaryTree/TreeEnumerator.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BinaryTree
+{
+    class TreeEnumerator<TItem> : IEnumerator<TItem> where TItem : IComparable<TItem>
+    {
+        private Tree<TItem> currentData = null;
+        private TItem currenItem = default(TItem);
+        private Queue<TItem> enumData = null;
+
+        TItem IEnumerator<TItem>.Current
+        {
+            get
+            {
+                if (this.enumData == null)
+                {
+                    throw new InvalidOperationException("Use MoveNext before calling Current");
+                }
+                return this.currenItem;
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public TreeEnumerator(Tree<TItem> data)
+        {
+            this.currentData = data;
+        }
+
+        private void populate(Queue<TItem> enumQueue, Tree<TItem> tree)
+        {
+            if (tree.LeftTree != null)
+            {
+                populate(enumQueue, tree.LeftTree);
+            }
+
+            enumQueue.Enqueue(tree.NodeData);
+
+            if (tree.RightTree != null)
+            {
+                populate(enumQueue, tree.RightTree);
+            }
+        }
+
+        bool IEnumerator.MoveNext()
+        {
+            if (this.enumData == null)
+            {
+                this.enumData = new Queue<TItem>();
+                populate(this.enumData, this.currentData);
+            }
+
+            if (this.enumData.Count > 0)
+            {
+                this.currenItem = this.enumData.Dequeue();
+                return true;
+            }
+            return false;
+        }
+
+        void IEnumerator.Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~TreeEnumerator() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        void IDisposable.Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
